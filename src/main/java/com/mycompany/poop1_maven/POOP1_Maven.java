5c97ee131b6d90acfff4f48c16adecc4d78b7e79
TonaGaMo <111716034+TonaGaMo@users.noreply.github.com>
2023-10-12 21:36:24 -0600

    Creación de la practica java con maven

---
+++ src/main/java/com/mycompany/poop1_maven/POOP1_Maven.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven;
+
+/**
+ *
+ * @author Asus
+ */
+public class POOP1_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde POO grupo 8 com maven");
+    }
+    
+}
